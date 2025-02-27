81423daa03f0ab7ffd07ef2ad654a49322154e83
leontoys <svliyon@gmail.com>
2025-02-27 11:42:56 +1100

    moved-code-to-App

--- part7/src/App.jsx
+++ part7/src/App.jsx
@@ -1,9 +1,66 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 
-function App() {
+import { useState } from "react";
+import ReactDOM from "react-dom/client"
+import { createRoot } from 'react-dom/client'
+
+
+//create components
+const Home = ()=> (
+  <div><h2>TKTL notes app</h2></div>
+)
+
+const Notes = ()=>(
+  <div><h2>Notes</h2></div>
+)
+
+const Users = ()=>(
+  <div><h2>Users</h2></div>
+)
+
+const App = ()=>{
+
+  //just a state for page
+  const [page,setPage] = useState('home')
+
+  const toPage = (page) => (event) => {
+    event.preventDefault()
+    setPage(page)
+  }
+
+  const content = () => {
+    if(page==='home'){
+      return <Home/>
+    }
+    else if(page==='notes'){
+      return <Notes/>
+    }
+    else if(page==='users'){
+      return <Users/>
+    }
+  }
+
+  const padding = {
+    padding : 5
+  }
+
+  return(
+    <div>
+      <div>
+        <a href="" onClick={toPage('home')} style={padding}>home</a>
+        <a href="" onClick={toPage('notes')} style={padding}>notes</a>
+        <a href="" onClick={toPage('users')} style={padding}>users</a>
+        {content()}
+      </div>
+    </div>
+  )
+
+}
+
+/* function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -30,6 +87,6 @@ function App() {
       </p>
     </>
   )
-}
+} */
 
 export default App
--- part7/src/main.jsx
+++ part7/src/main.jsx
@@ -1,71 +1,10 @@
-/* import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-) */
-
-import { useState } from "react";
-import ReactDOM from "react-dom/client"
-import { createRoot } from 'react-dom/client'
-
-
-//create components
-const Home = ()=> (
-  <div><h2>TKTL notes app</h2></div>
 )
-
-const Notes = ()=>(
-  <div><h2>Notes</h2></div>
-)
-
-const Users = ()=>(
-  <div><h2>Users</h2></div>
-)
-
-const App = ()=>{
-
-  //just a state for page
-  const [page,setPage] = useState('home')
-
-  const toPage = (page) => (event) => {
-    event.preventDefault()
-    setPage(page)
-  }
-
-  const content = () => {
-    if(page==='home'){
-      return <Home/>
-    }
-    else if(page==='notes'){
-      return <Notes/>
-    }
-    else if(page==='users'){
-      return <Users/>
-    }
-  }
-
-  const padding = {
-    padding : 5
-  }
-
-  return(
-    <div>
-      <div>
-        <a href="" onClick={toPage('home')} style={padding}>home</a>
-        <a href="" onClick={toPage('notes')} style={padding}>notes</a>
-        <a href="" onClick={toPage('users')} style={padding}>users</a>
-        {content()}
-      </div>
-    </div>
-  )
-
-}
-
-createRoot(document.getElementById('root')).render(
-    <App />
-) 
